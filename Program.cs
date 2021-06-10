04ea816daab058182526c8842da21a1de9d25aad
Vassterak <vaclavhorcicka@outlook.cz>
2021-06-10 11:35:36 +0200

    Přidejte soubory projektu.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGame_Pexeso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
